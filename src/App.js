3b6a670ca359a64703a806ea45cac28621c3b01b
sakshiarora28 <arorasakshi1589@gmail.com>
2024-08-01 10:59:55 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Footer from "./components/footer";
 import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
     <Header />
+    <Container>
+      <main>
+        
+      </main>
+    </Container>
+    <Footer/>
     </>
   );
 }
